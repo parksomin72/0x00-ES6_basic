3c4d79303381535d75895ca8eec2d13895286205
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-08 09:46:20 +0000

    Object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,4 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = {
-    income: income,
-    gdp: gdp,
-    capita: capita,
-  };
-
+  const budget = { income, gdp, capita };
   return budget;
 }

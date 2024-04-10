4055c975a52e1dfd244a07a2c4f08a92369cd8b8
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-10 05:35:47 +0000

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,19 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
-  
-  try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.toString());
-  } finally {
-    queue.push('Guardrail was processed');
-  }
-  
-  return queue;
+	const queue = [];
+
+	try
+	{
+		const result = mathFunction();
+		queue.push(result);
+	}
+	catch (error)
+	{
+		queue.push(error.toString());
+	}
+	finally
+	{
+		queue.push('Guardrail was processed');
+	}
+
+	return queue;
 }

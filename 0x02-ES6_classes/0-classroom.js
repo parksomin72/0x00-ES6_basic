4a2fe7b6d3c9daaec9310174d7059dec368f2304
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-12 01:03:49 +0000

    You used to attend a place like this at some point

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,20 +2,13 @@
  * Class representing a classroom with a maximum
  * number of students.
  */
-export default class ClassRoom
-{
+export default class ClassRoom {
 	/**
 	 * Creates a new ClassRoom instance.
 	 * @param {Number} maxStudentsSize - The maximum number of
 	 * students allowed in the class.
 	 */
-	constructor(maxStudentsSize)
-	{
-		/**
-		 * The maximum number of students allowed in the class.
-		 * @type {Number}
-		 * @private
-		 */
+	constructor(maxStudentsSize) {
 		this._maxStudentsSize = maxStudentsSize;
 	}
 }

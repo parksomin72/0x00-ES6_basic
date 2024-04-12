bff5b29f73d2855cea54394acd1a20f57b7de635
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-12 02:43:02 +0000

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,20 +1,18 @@
 import Car from './10-car.js';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range; // Use _range for consistency
-  }
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
 
-  get range() {
-    return this._range;
-  }
+    get range() {
+        return this._range;
+    }
 
-  set range(value) {
-    this._range = value;
-  }
-
-  cloneCar() {
-    return new this.constructor(this.brand, this.motor, this.color, this.range);
-  }
+    cloneCar() {
+        const carCopy = new Car(this._brand, this._motor, this._color);
+        return carCopy;
+    }
 }
+
